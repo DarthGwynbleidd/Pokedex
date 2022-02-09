c69f6986d02f842e17a62001fca36f151278b7df
vincent <vincentcharlettepro@gmail.com>
2022-02-09 16:02:42 +0100

    header component

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -1,10 +1,12 @@
+import Header from "./components/header.js"
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="container">
+        <Header />
+      </div>
     </div>
   );
 }
