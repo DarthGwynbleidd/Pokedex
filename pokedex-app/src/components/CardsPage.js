673d82fff8e3e6ab81675337ae354ef6057432b9
vincent <vincentcharlettepro@gmail.com>
2022-02-10 11:30:47 +0100

    resolve conflicts

---
+++ pokedex-app/src/components/CardsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopButtons from './TopButtons';
+
+const CardsPage = () => {
+    return (
+        <div>
+            <TopButtons />
+        </div>
+    );
+};
+
+export default CardsPage;
---
+++ pokedex-app/src/components/TopButtons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const TopButtons = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
+
+export default TopButtons;
