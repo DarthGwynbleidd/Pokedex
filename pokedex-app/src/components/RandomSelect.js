46c8ef3fa55d32af7b7fb271d4d211d188632d93
AngéliqueRioux <allange002@gmail.com>
2022-02-11 11:01:58 +0100

    html-css completed

--- pokedex-app/src/components/RandomSelect.js
+++ pokedex-app/src/components/RandomSelect.js
@@ -1,22 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const RandomSelect = () => {
-    const [isLabelClick,setIsLabelClick]= useState(false)
+    
     return (
         <div className='randomSelect'>
             
             <button className='randomSelect__button'><img src="./assets/top-buttons/random-logo.png" alt='random'/><span className='randomSelect__button__p'>Surprise</span></button>
-            
-            <select className='randomSelect__select'>    
+            <div className='randomSelect__wrapper'>
+            <img className='randomSelect__wrapper__pokeball' src='./assets/top-buttons/select-logo.png' alt='pokeball'/>
+            <select className='randomSelect__wrapper__select'>    
 
                 <option value="search by number">Tri par ordre croissant</option>
                 <option value="search by number">Tri par ordre décroissant</option>
-                <option value="search by A-Z">Tri par ordre alphabétique</option>
+                <option value="search by A-Z">Tri de A-Z</option>
                 <option value="search by Z-A">Tri de Z-A</option>
         
             </select>
-            <div className='randomSelect__label' tabIndex={0} >
-                <label>Tri par ordre croissant</label>
+
             </div>
         </div>
     );
