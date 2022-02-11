bd80e6462f7ebc89293d904ebd477dae296dc57c
micky06 <michael-michaux_student2021@wilder.school>
2022-02-11 10:24:23 +0100

    create a rounded  border

--- pokedex-app/src/components/CardsPage.js
+++ pokedex-app/src/components/CardsPage.js
@@ -3,7 +3,12 @@ import React from 'react';
 const CardsPage = () => {
     return (
         <div>
-            
+                <div className='borderTop'>
+                    <div className='borderTop-left'></div>
+                    <div className='borderTop-center'></div>
+                    <div className='borderTop-right'></div>
+                </div>
+  
         </div>
     );
 };
