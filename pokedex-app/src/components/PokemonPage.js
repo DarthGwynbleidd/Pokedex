04811dfd84924e05f63d519556d9d3227e207d83
AngéliqueRioux <allange002@gmail.com>
2022-02-16 11:54:01 +0100

    creation Pokemon & Evolutions components

--- pokedex-app/src/components/PokemonPage.js
+++ pokedex-app/src/components/PokemonPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Evolutions from './Evolutions';
 import NavigBar from './NavigBar';
+import Pokemon from './Pokemon';
 
 const PokemonPage = () => {
     return (
         <div className='pokemonPage'>
            <NavigBar />
+           <Pokemon />
+           <Evolutions />
            
         </div>
     );
