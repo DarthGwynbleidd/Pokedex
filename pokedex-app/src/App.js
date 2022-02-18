3c67b8200aafdac8f546e78d4d4b6a45ebcaed7d
micky06 <michael-michaux_student2021@wilder.school>
2022-02-18 12:33:12 +0100

    create api & context

--- pokedex-app/src/App.js
+++ pokedex-app/src/App.js
@@ -4,21 +4,34 @@ import CardsPage from "./components/CardsPage";
 import PokemonPage from "./components/PokemonPage";
 import NotFound from "./components/NotFound";
 import { Routes, Route } from "react-router-dom";
+import PokemonContext from "./contexts/PokemonContext";
+import {useState} from 'react';
 
 
 function App() {
+  const [pokemon, setPokemon] = useState([
+    {
+      name: '', 
+      number: '',
+      image: '',
+      type1: '',
+      type2: ''
+    }
+  ])
   return (
-    <div className="App">
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path="/" element={<CardsPage />} />
-          <Route path="/:name" element={<PokemonPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
+    <PokemonContext.Provider value={{pokemon: pokemon, setPokemon: setPokemon}}>
+      <div className="App">
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<CardsPage />} />
+            <Route path="/:name" element={<PokemonPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </div>
       </div>
-    </div>
+    </PokemonContext.Provider>
   );
 }
 
