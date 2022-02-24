a27b5a18a143bf50bd8b3a0508997defc1941f40
vincent <vincentcharlettepro@gmail.com>
2022-02-24 12:41:32 +0100

    add random button feature

--- pokedex-app/src/components/CardsPage.js
+++ pokedex-app/src/components/CardsPage.js
@@ -5,10 +5,12 @@ import TopButtons from './TopButtons';
 import Grid from './Grid';
 import BottomButton from './BottomButton';
 import PokemonContext from '../contexts/PokemonContext';
+import DisplayContext from '../contexts/DisplayContext';
 
 const CardsPage = () => {
     const { setPokemons } = useContext(PokemonContext);
     const [group, setGroup] = useState(1)
+    const [display, setDisplay] = useState("numericUp")
 
 
     useEffect(() => {
@@ -19,38 +21,57 @@ const CardsPage = () => {
             const responseLength = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/`)
             const length = responseLength.data.count
 
-            
-            for (let number = group; number < group + 12 ; number++) {
-                if(number===length + 1){
-                    break
-                }
-                    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${number}`)
-                
-                    const obj = { id: '', pokemonName: '', image: '', type1: '', type2: '' };
-                    obj.id = response.data.id;
-                    obj.pokemonName = response.data.name;
-                    obj.image = response.data.sprites.other["official-artwork"].front_default;
-                    obj.type1 = response.data.types[0].type.name;
-                    try {
-                        obj.type2 = response.data.types[1].type.name;
-                    } catch (error) {
 
+            for (let number = group; number < group + 12; number++) {
+                let alias = ''
+                if (display === 'random') {
+                    alias = Math.floor(Math.random() * (length - 1)) + 1
+                }
+                else if (display === 'numericUp') {
+                    alias = number
+                    if (number === length + 1) {
+                        break
                     }
-                    temp.push(obj)
-              
+                }
+                // else if (display === 'numericDown') {
+
+                // }
+                // else if (display === 'alphaUp') {
+
+                // }
+                // else if (display === 'alphaDown') {
+
+                // }
+
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${alias}`)
+
+                const obj = { id: '', pokemonName: '', image: '', type1: '', type2: '' };
+                obj.id = response.data.id;
+                obj.pokemonName = response.data.name;
+                obj.image = response.data.sprites.other["official-artwork"].front_default;
+                obj.type1 = response.data.types[0].type.name;
+                try {
+                    obj.type2 = response.data.types[1].type.name;
+                } catch (error) {
+
+                }
+                temp.push(obj)
+
             }
             setPokemons(prevPokemons => prevPokemons.concat(temp));
         }
         fetchPokemons();
-    }, [group, setPokemons]);
+    }, [group, setPokemons, display]);
 
     return (
-        <div className='cardsPage'>
-            <AdvancedResearch />
-            <TopButtons />
-            <Grid />
-            <BottomButton setGroup={setGroup} />
-        </div>
+        <DisplayContext.Provider value={{ display, setDisplay }}>
+            <div className='cardsPage'>
+                <AdvancedResearch />
+                <TopButtons />
+                <Grid />
+                <BottomButton setGroup={setGroup} />
+            </div>
+        </DisplayContext.Provider>
     );
 };
 
--- pokedex-app/src/components/RandomSelect.js
+++ pokedex-app/src/components/RandomSelect.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import DisplayContext from '../contexts/DisplayContext';
+import PokemonContext from '../contexts/PokemonContext';
 
 const RandomSelect = () => {
-    
+
+    const { display, setDisplay } = useContext(DisplayContext)
+    const { setPokemons } = useContext(PokemonContext)
+
+    const handleClick = () => {
+        setDisplay('random')
+        setPokemons([])
+    }
+
     return (
         <div className='randomSelect'>
-            
-            <button className='randomSelect__button'><img src="./assets/top-buttons/random-logo.png" alt='random'/><span className='randomSelect__button__p'>Surprise</span></button>
-            <div className='randomSelect__wrapper'>
-            <img className='randomSelect__wrapper__pokeball' src='./assets/top-buttons/select-logo.png' alt='pokeball'/>
-            <select className='randomSelect__wrapper__select'>    
 
-                <option value="search by number">Tri par ordre croissant</option>
-                <option value="search by number">Tri par ordre décroissant</option>
-                <option value="search by A-Z">Tri de A-Z</option>
-                <option value="search by Z-A">Tri de Z-A</option>
-        
-            </select>
+            <button onClick={handleClick} className='randomSelect__button'><img src="./assets/top-buttons/random-logo.png" alt='random' />
+                <span className='randomSelect__button__p'>Surprise</span></button>
+            <div className='randomSelect__wrapper'>
+                <img className='randomSelect__wrapper__pokeball' src='./assets/top-buttons/select-logo.png' alt='pokeball' />
+                <select className='randomSelect__wrapper__select'>
+
+                    <option value="search by number">Choisissez une option</option>
+                    <option value="search by number" selected>Tri par ordre croissant</option>
+                    <option value="search by number">Tri par ordre décroissant</option>
+                    <option value="search by A-Z">Tri de A-Z</option>
+                    <option value="search by Z-A">Tri de Z-A</option>
+
+                </select>
 
             </div>
         </div>
